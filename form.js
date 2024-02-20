bfed386a2ea3fa519237541ca24673b4d47ec0fd
Hovsep Stepanian <79714628+HOVSEP93@users.noreply.github.com>
2024-02-20 14:16:00 +1100

    Update form.js

--- form.js
+++ form.js
@@ -1,21 +1,11 @@
-function calculateResult() {
-  var amount = parseFloat(document.getElementById(111863965).value);
-  var term = parseInt(document.getElementById(111480652).value);
-
-  if (isNaN(amount) || isNaN(term) || term <= 0) {
-    // If either amount or term is not a number or term is less than or equal to 0, exit
-    return;
-  }
-
-  var result = amount / (term * 26);
-  var roundedResult = Math.ceil(result * 100) / 100; // Rounding up to two decimal places
-
-  document.getElementById(111480518).value = roundedResult.toFixed(2); // Displaying rounded result with two decimal places
+function roundResult() {
+  var result1 = loader.engine.document
+    .getElementById(111480518)
+    .getProperty("value.value");
+  var roundedValue1 = +(Math.round(result1 * 100) / 100).toFixed(2);
+  loader.engine.document
+    .getElementById(111480518)
+    .setValue({ value: roundedValue1 });
 }
 
-// Call the calculateResult function whenever there is a change in Field A or Field B
-document.getElementById(111863965).addEventListener("input", calculateResult);
-document.getElementById(111480652).addEventListener("input", calculateResult);
-
-// Initial calculation when the page loads
-calculateResult();
+roundResult()
