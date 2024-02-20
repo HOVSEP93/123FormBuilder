3d9a687a2e5038024f8b4ed6d6b8863477b87f4e
Hovsep Stepanian <79714628+HOVSEP93@users.noreply.github.com>
2024-02-20 13:16:57 +1100

    Update form.js

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
 // Get the value from the field with ID 111480518
-var fieldValue = parseFloat(document.getElementById("111996807").textContent);
+var fieldValue = parseFloat(document.getElementById("111997141").textContent);
 
 // Round the field value to two decimal places using Math.round()
 var roundedValue = Math.round(fieldValue * 100) / 100;
