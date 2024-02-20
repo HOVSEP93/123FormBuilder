69ec528aebd58ad0fe8440c15e130cc1e45d17c1
Hovsep Stepanian <79714628+HOVSEP93@users.noreply.github.com>
2024-02-20 13:42:53 +1100

    Update form.js

--- form.js
+++ form.js
@@ -1,11 +1,13 @@
 function roundResult() {
-  var result1 = loader.engine.document
-    .getElementById(111480518)
-    .getProperty("value.value");
+  // Retrieve the value of the field
+  var result1 = parseFloat(loader.engine.document.getElementById('111480518').getProperty("value.value"));
+
+  // Round the value to two decimal places
   var roundedValue1 = +(Math.round(result1 * 100) / 100).toFixed(2);
-  loader.engine.document
-    .getElementById(111480518)
-    .setValue({ value: roundedValue1 });
+
+  // Set the rounded value back to the field
+  loader.engine.document.getElementById('111480518').setValue({ value: roundedValue1 });
 }
 
-window.onchange = roundResult;
+// Attach the roundResult function to the onchange event of the field
+loader.engine.document.getElementById('111480518').onchange = roundResult;
